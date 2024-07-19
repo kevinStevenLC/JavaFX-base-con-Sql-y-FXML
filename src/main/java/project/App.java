757402930bf9baa6c90156ff3a4e0c5310e9dea4
kevinStevenLC <kevinlema768xd@gmail.com>
2024-07-18 23:19:44 -0500

    commit de actualizacion del proyecto como base

--- src/main/java/project/App.java
+++ src/main/java/project/App.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import project.controller.PanelController;
 
 /**
  * JavaFX App
@@ -15,32 +16,25 @@ public class App extends Application {
 
     @Override
     public void start(@SuppressWarnings("exports") Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/fxml/ventana1.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/fxml/pagePanel.fxml"));
         Parent root = loader.load();
         stage.setScene(new Scene(root));
         stage.setTitle("Panel de control");
-
+        // Panel de control
+        PanelController controller = loader.getController();
+        // Enviando panel de control.
+        controller.recivePanel(stage);
+        // seguir enviando escenarios
         stage.show();
     }
 
     @SuppressWarnings("exports")
-    public Stage FxmlVentana1() throws IOException {
+    public Stage GenerateFxmlTable(String file, String title) throws IOException {
         Stage stage = new Stage();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/fxml/tableDrivers.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/fxml/" + file + ".fxml"));
         Parent root = loader.load();
         stage.setScene(new Scene(root));
-        stage.setTitle("ventana 2");
-
-        return stage;
-    }
-
-    @SuppressWarnings("exports")
-    public Stage FxmlVentana2() throws IOException {
-        Stage stage = new Stage();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/fxml/tableConstructors.fxml"));
-        Parent root = loader.load();
-        stage.setScene(new Scene(root));
-        stage.setTitle("ventana 3");
+        stage.setTitle(title);
 
         return stage;
     }
--- src/main/java/project/repository/ModeloRepository.java
+++
@@ -1,5 +0,0 @@
-package project.repository;
-
-public class ModeloRepository {
-
-}
